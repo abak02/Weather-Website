a17925185bfb77c3d1b1481e0c9d14a2155cef1a
Fahim Foysal Abak <abakfahim@gmail.com>
2022-02-20 18:26:30 +0600

    5 days forecast added

--- App.js
+++ App.js
@@ -16,7 +16,6 @@ showWeatherInfo("dhaka");
             let allData = data[0];
             lat=allData.lat;
             lon= allData.lon;
-           
             fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=5ab045bbcf781f7a13344f5061a86f81")
             .then(res=>res.json())
             .then(data=>{
@@ -30,8 +29,11 @@ showWeatherInfo("dhaka");
             const snow = document.getElementById("snow");
             const bright = document.getElementById("bright");
             const cloud = document.getElementById("cloud");
+            const weather = document.getElementById("weather");
+            const icon=document.getElementById("icon");
+            const rain=document.getElementById("rain");
             const windData=data.wind.speed;
-            wind.innerText=windData+" m/s";
+            wind.innerText=parseFloat(windData*3.6).toFixed(2)+" km/h";
             const humidity=data.main.humidity;
             snow.innerText=humidity+" %";
             const sunrise=data.sys.sunrise;
@@ -41,6 +43,51 @@ showWeatherInfo("dhaka");
             var seconds = date.getSeconds();
             bright.innerText= hours+' : '+minutes+' : '+seconds;
             cloud.innerText=data.clouds.all+" %";
+            weather.innerText=data.weather[0].main;
+            const url = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+            icon.src = url;
+            document.getElementById("feel").innerText=parseInt(data.main.feels_like-273.16);
+            //rain.innerText=data.rain.1h;
+
+
+            
+
+
+
         })
+            //5-day forecast
+            fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=5ab045bbcf781f7a13344f5061a86f81")
+            .then(res=>res.json())
+            .then(data=>showWeatherForecast(data));
+            function showWeatherForecast(data){
+                let weatherHTML="";
+                for (let i = 0; i < data.list.length; i++) {
+                    const element = data.list[i];
+                    console.log(element);
+        
+                    weatherHTML=weatherHTML+`
+                   
+                        <div class="col-md-3 mt-4">
+                            <div class="box">
+                                <div class="d-flex justify-content-between>
+                                <p class="text-muted fw-bold text-size-2">Date : ${element.dt_txt.slice(0,10)} Time : ${element.dt_txt.slice(11,19)}</p>
+                                </div>
+                                
+                                <h1 class="text-center mb-0">${parseInt(element.main.temp-273.16)}° </h1>
+                                <h6 class="text-center text-muted">Feels Like : ${parseInt(element.main.feels_like-273.16)}° </h6>
+                                <h6 class="text-center text-muted">${element.weather[0].main} </h6>
+                            
+                                <div class="d-flex justify-content-between">
+                                    <img src="images/snow-new.svg"><span id="snow" class="ps-1 text-size text-muted">${element.main.humidity}%</span>
+                                    <img src="images/wind-new.svg"><span id="wind" class="ps-1 text-size text-muted">${element.wind.speed}km/h</span>
+                                    <img src="images/cloud-new.svg"><span id="cloud" class="ps-1 text-size text-muted">${element.clouds.all}%</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    `
+                }
+                document.getElementById("forecast").innerHTML=weatherHTML;
+            }
         })
         }
